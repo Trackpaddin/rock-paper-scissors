da349a77d01833bb9a4ded9553465b2e2f681f22
Thomas Knowlton <129105603+Trackpaddin@users.noreply.github.com>
2025-07-09 01:37:20 -0500

    Add computer choice logic

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    if (randomChoice === 0) {
+        return "rock";
+    } else if (randomChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComChoice());
